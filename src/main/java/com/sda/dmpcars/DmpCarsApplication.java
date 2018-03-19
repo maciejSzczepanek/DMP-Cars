0178e86d63cf94395b2aa55f207ee2cdca96c9d2
dk <dawid.dk6@gmail.com>
2018-03-19 19:26:59 +0100

    Spring boot starter.

---
+++ src/main/java/com/sda/dmpcars/DmpCarsApplication.java
@@ -0,0 +1,12 @@
+package com.sda.dmpcars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DmpCarsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DmpCarsApplication.class, args);
+	}
+}
